b3eb2a5a7481720546578a188778f76afe66a9b1
reineltmend <reinel@mend.com>
2022-03-30 17:56:56 -0600

    - Created superadmin user to access to admin panel - Created Student Model

--- pythonAPI/api/models.py
+++ pythonAPI/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    FirstName = models.CharField(max_length=60)
+    LastName = models.CharField(max_length=60)
+    Email = models.CharField(max_length=320)
+    Major = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.FirstName + " " + self.LastName
