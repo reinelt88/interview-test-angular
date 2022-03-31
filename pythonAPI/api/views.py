1160a2c35741e93ecccdabb97b26c0cbb96fae46
reineltmend <reinel@mend.com>
2022-03-30 18:18:28 -0600

    - Added REST urls - Added serialized view

--- pythonAPI/api/views.py
+++ pythonAPI/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import StudentSerializer
+from .models import Student
+
 
 # Create your views here.
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all().order_by('FirstName')
+    serializer_class = StudentSerializer
