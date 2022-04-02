ad5f91249e66f8dc19c0b32f0561c661c778619e
reineltmend <reinel@mend.com>
2022-04-01 20:40:12 -0600

    - Fixed bug

--- pythonAPI/api/urls.py
+++ pythonAPI/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path(r'students/', views.student_list, name='student_list'),
-    path(r'students/create', views.student_create, name='student_create'),
+    path(r'students/create/', views.student_create, name='student_create'),
     path(r'students/detail/<int:pk>/', views.student_detail, name='student_detail'),
     path(r'students/update/<int:pk>/', views.student_update, name='student_update'),
     path(r'students/delete/<int:pk>/', views.student_delete, name='student_delete'),
